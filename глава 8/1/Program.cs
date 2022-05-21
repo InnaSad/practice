e8ac2b1e05fb5c76bd35faf6357745726c6222a2
InnaSad <innasadkovskaa@gmail.com>
2022-05-21 10:52:15 +0300

    глава 8

---
+++ глава 8/1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        const int STUDENTS_QUANTITY = 5;
+        const int SUBJECT_QUANTITY = 5;
+        static void Main(string[] args)
+        {
+            Student[] Results = new Student[STUDENTS_QUANTITY];
+            GetStudents(Results);
+            CheckGoodMark(Results);
+            Console.ReadLine();
+        }
+        public struct Student
+        {
+            public string Surname;
+            public string Initials;
+            public int GroupNumber;
+            public int[] Marks;
+        }
+        static void GetStudents(Student[] Students)
+        {
+            int number;
+            bool converted;
+            Student student = new Student();
+            student.Marks = new int[SUBJECT_QUANTITY];
+            for (int i = 0; i < STUDENTS_QUANTITY; i++)
+            {
+                Console.Write("Введите фамилию студента: ");
+                student.Surname = Console.ReadLine();
+                Console.Write("Введите инициалы студента: ");
+                student.Initials = Console.ReadLine();
+                do
+                {
+                    Console.Write("Введите группу студента: ");
+                    converted = int.TryParse(Console.ReadLine(), out number);
+                }
+                while (converted == false);
+                student.GroupNumber = number;
+                Console.WriteLine("Введите оценки студента: ", SUBJECT_QUANTITY);
+                for (int h = 0; h < SUBJECT_QUANTITY; h++)
+                {
+                    do
+                    {
+                        Console.Write("{0}) ", h + 1);
+                        converted = int.TryParse(Console.ReadLine(), out number);
+                    }
+                    while (converted == false);
+                    student.Marks[h] = number;
+                }
+                Students[i] = student;
+                Console.WriteLine();
+            }
+            Array.Sort(Students, (x, y) => x.GroupNumber == y.GroupNumber ? 0 : x.GroupNumber > y.GroupNumber ? 1 : -1);
+        }
+        static void CheckGoodMark(Student[] Students)
+        {
+            bool found = false;
+            foreach (Student student in Students)
+            {
+                if (student.Marks.Average() >= 4.0)
+                {
+                    found = true;
+                    Console.WriteLine("Студент {0} {1} из группы {2} имеет средний бал больше 4,0",
+                        student.Surname, student.Initials, student.GroupNumber);
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Записей не найдено");
+            }
+        }
+    }
+}
---
+++ глава 8/2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2
+{
+    class Program
+    {
+        enum Employs
+        {
+            Manager = 200,
+            Teacher = 310,
+            Director = 270
+        };
+        static class Accauntant
+        {
+            public static bool AskForBonus(Employs emp, int workedHours)
+            {
+                return (int)emp < workedHours;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var empHours = new Dictionary<Employs, int>
+            {
+                [Employs.Manager] = 200,
+                [Employs.Teacher] = 330,
+                [Employs.Director] = 250
+            };
+            foreach (var pair in empHours)
+            {
+                Console.WriteLine($"{ pair.Key }: { Accauntant.AskForBonus(pair.Key, pair.Value) }");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
