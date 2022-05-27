5132833c946d8b3c9fbc725dd023e8b7a86ec0d8
InnaSad <innasadkovskaa@gmail.com>
2022-05-27 15:02:34 +0300

    глава 13

---
+++ глава 13/1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        public delegate void Color(string color);
+        static void Red(string color)
+        {
+            color = "Красный";
+            Console.WriteLine(color);
+        }
+        static void Green(string color)
+        {
+            color = "Зеленый";
+            Console.WriteLine(color);
+        }
+        static void Blue(string color)
+        {
+            color = "Синий";
+            Console.WriteLine(color);
+        }
+        static void Black(string color, Color bl)
+        {
+            color = "Черный";
+            bl(color);
+        }
+        static void Main(string[] args)
+        {
+            Color color = null;
+            color += Red;
+            color += Green;
+            color += Blue;
+            color("");
+            Color black = Console.WriteLine;
+            Black("", black);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ глава 13/2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2
+{
+    public class MyInfo
+    {
+        public event EventHandler<NameChange> nameEvent = delegate { };
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = "Вася";
+                nameEvent(this, new NameChange(string.Format("Имя было изменено с {0} на {1}", name, value)));
+                name = value;
+            }
+        }
+    }
+    public class NameChange 
+    {
+        public readonly string Message;
+        public NameChange(string msg)
+        {
+            Message = msg;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string n = Console.ReadLine();
+            MyInfo p = new MyInfo();
+            p.nameEvent += new EventHandler<NameChange>(p_nameEvent);
+            p.Name = n;
+            Console.ReadLine();
+        }
+        static void p_nameEvent(object sender, NameChange e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
