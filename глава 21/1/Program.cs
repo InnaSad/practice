374eedd88349af50cd0f7a81fb2697e5543f855e
InnaSad <innasadkovskaa@gmail.com>
2022-06-06 13:54:17 +0300

    глава 21

---
+++ глава 21/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread process1 = new Thread(() =>
+            {
+                Singleton singleton = Singleton.GetInstance("Арбуз");
+                Console.WriteLine(singleton.Value);
+            });
+            Thread process2 = new Thread(() =>
+            {
+                Singleton singleton = Singleton.GetInstance("Дыня");
+                Console.WriteLine(singleton.Value);
+            });
+            process1.Start();            
+            process1.Join();
+            process2.Start();
+            process2.Join();
+            Console.ReadLine();
+        }
+    }    
+}
---
+++ глава 21/1/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Singleton
+    {
+        private Singleton() { }
+        private static Singleton _instance;
+        private static readonly object _lock = new object();
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+        public string Value { get; set; }
+    }
+}
