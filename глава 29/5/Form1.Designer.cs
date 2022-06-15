b5885adbf3c7a47a48668df906a6469523f1bd31
InnaSad <innasadkovskaa@gmail.com>
2022-06-15 13:05:36 +0300

    глава 29

---
+++ глава 29/5/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace _5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(421, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(547, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Задание 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ глава 29/5/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x, y, y1;
+        int i = 0, j = 0;
+        SolidBrush B = new SolidBrush(Color.Blue);
+        SolidBrush R = new SolidBrush(Color.DarkRed);
+        SolidBrush U = new SolidBrush(Color.OrangeRed);
+        Point[] T = new Point[4];
+        Point[] L = new Point[7];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            y = 220;
+            x = 160;
+            y1 = 256;
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (i == 1)
+            {
+                L[0] = new Point(x + 15, y1);
+                L[1] = new Point(x + 10, y1 + 20);
+                L[2] = new Point(x + 15, y1 + 15);
+                L[3] = new Point(x + 20, y1 + 30);
+                L[4] = new Point(x + 25, y1 + 15);
+                L[5] = new Point(x + 30, y1 + 20);
+                L[6] = new Point(x + 25, y1);
+                g.FillPolygon(U, L);
+            }
+            T[0] = new Point(x + 20, y);
+            T[1] = new Point(x, y + 50);
+            T[2] = new Point(x + 20, y + 40);
+            T[3] = new Point(x + 40, y + 50);
+            g.FillPolygon(R, T);
+            g.FillEllipse(B, x + 16, y + 15, 8, 8);
+            g.FillEllipse(B, x + 16, y + 25, 8, 8);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i == 1)
+            {
+                if (j % 2 != 0)
+                {
+                    y1 += 3;
+                }
+                else
+                    y1 -= 3;
+                j++;
+                y1 -= 3;
+                y -= 3;
+                Invalidate();
+            }
+        }
+    }
+}
