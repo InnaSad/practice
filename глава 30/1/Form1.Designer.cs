4353d4da67d1bec80c4706b1eb5af09dc89572e3
InnaSad <innasadkovskaa@gmail.com>
2022-06-17 11:28:15 +0300

    глава 30

---
+++ глава 30/1/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dBTur_firmDataSet = new _1.DBTur_firmDataSet();
+            this.dBTurfirmDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ТуристыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.турыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 299);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // dBTur_firmDataSet
+            // 
+            this.dBTur_firmDataSet.DataSetName = "DBTur_firmDataSet";
+            this.dBTur_firmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dBTurfirmDataSetBindingSource
+            // 
+            this.dBTurfirmDataSetBindingSource.DataSource = this.dBTur_firmDataSet;
+            this.dBTurfirmDataSetBindingSource.Position = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ТуристыToolStripMenuItem,
+            this.турыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ТуристыToolStripMenuItem
+            // 
+            this.ТуристыToolStripMenuItem.Name = "ТуристыToolStripMenuItem";
+            this.ТуристыToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.ТуристыToolStripMenuItem.Text = "Туристы";
+            this.ТуристыToolStripMenuItem.Click += new System.EventHandler(this.туристыToolStripMenuItem_Click);
+            // 
+            // турыToolStripMenuItem
+            // 
+            this.турыToolStripMenuItem.Name = "турыToolStripMenuItem";
+            this.турыToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.турыToolStripMenuItem.Text = "Туры";
+            this.турыToolStripMenuItem.Click += new System.EventHandler(this.турыToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(317, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить туриста";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(132, 369);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Код туриста";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(39, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Фамилия";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(132, 402);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(84, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Имя";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(132, 435);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 27);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(35, 471);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Отчество";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(132, 471);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 27);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(317, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 36);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Изменить туриста";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(564, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 34);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Удалить тур";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 517);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Туристическая фирма";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dBTurfirmDataSetBindingSource;
+        private DBTur_firmDataSet dBTur_firmDataSet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ТуристыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem турыToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ глава 30/1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        public Form1()
+        {
+            InitializeComponent();
+            string ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; " + "Data Source=DBTur_firm.mdb";
+            conn = new OleDbConnection(ConnectionString);
+        }
+
+        void GetTourists()
+        {           
+            conn.Open();
+            da = new OleDbDataAdapter("SELECT *FROM Туристы", conn);
+            ds = new DataSet();
+            da.Fill(ds, "Туристы");
+            dataGridView1.DataSource = ds.Tables["Туристы"];
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+                column.Width = 132;
+            conn.Close();
+        }
+
+        void GetTours()
+        {
+            conn.Open();
+            da = new OleDbDataAdapter("SELECT *FROM Туры", conn);
+            ds = new DataSet();
+            da.Fill(ds, "Туры");
+            dataGridView1.DataSource = ds.Tables["Туры"];
+            dataGridView1.Columns[0].Width = 80;
+            dataGridView1.Columns[1].Width = 80;
+            dataGridView1.Columns[2].Width = 80;
+            dataGridView1.Columns[3].Width = 289;
+            conn.Close();
+        }
+
+        private void туристыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetTourists();
+        }
+
+        private void турыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetTours();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From Туры Where Код_тура=@id";
+            cmd = new OleDbCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells[0].Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            GetTours();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "Insert into Туристы (Фамилия,Имя,Отчество) values (@fName,@lName,@pName)";
+            cmd = new OleDbCommand(query, conn);
+            cmd.Parameters.AddWithValue("@fName", textBox2.Text);
+            cmd.Parameters.AddWithValue("@lName", textBox3.Text);
+            cmd.Parameters.AddWithValue("@pName", textBox4.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            GetTourists();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "Update Туристы Set Фамилия=@fName,Имя=@lName,Отчество=@pName Where Код_туриста=@id";
+            cmd = new OleDbCommand(query, conn);
+            cmd.Parameters.AddWithValue("@fName", textBox2.Text);
+            cmd.Parameters.AddWithValue("@lName", textBox3.Text);
+            cmd.Parameters.AddWithValue("@pName", textBox4.Text);
+            cmd.Parameters.AddWithValue("@id", Convert.ToInt32(textBox1.Text));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            GetTourists();
+        }
+    }
+}
