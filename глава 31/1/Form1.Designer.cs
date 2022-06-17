2b7f8c41fe29a6fb1226440b09a7992f08869ac9
InnaSad <innasadkovskaa@gmail.com>
2022-06-17 13:05:21 +0300

    глава 31

---
+++ глава 31/1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dBTur_firmDataSet = new _1.DBTur_firmDataSet();
+            this.dBTurfirmDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.туристыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.турыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Wheat;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(674, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dBTur_firmDataSet
+            // 
+            this.dBTur_firmDataSet.DataSetName = "DBTur_firmDataSet";
+            this.dBTur_firmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dBTurfirmDataSetBindingSource
+            // 
+            this.dBTurfirmDataSetBindingSource.DataSource = this.dBTur_firmDataSet;
+            this.dBTurfirmDataSetBindingSource.Position = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.туристыToolStripMenuItem,
+            this.турыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(691, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // туристыToolStripMenuItem
+            // 
+            this.туристыToolStripMenuItem.Name = "туристыToolStripMenuItem";
+            this.туристыToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.туристыToolStripMenuItem.Text = "Туристы";
+            this.туристыToolStripMenuItem.Click += new System.EventHandler(this.туристыToolStripMenuItem_Click);
+            // 
+            // турыToolStripMenuItem
+            // 
+            this.турыToolStripMenuItem.Name = "турыToolStripMenuItem";
+            this.турыToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.турыToolStripMenuItem.Text = "Туры";
+            this.турыToolStripMenuItem.Click += new System.EventHandler(this.турыToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить туриста";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Изменить туриста";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить тур";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(691, 374);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Задание 1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dBTurfirmDataSetBindingSource;
+        private DBTur_firmDataSet dBTur_firmDataSet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem туристыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem турыToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ глава 31/1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        string commandText;
+        OleDbConnection conn;
+        OleDbCommand MyCommand;
+        OleDbDataAdapter dataAdapter;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; " + "Data Source=DBTur_firm.mdb";
+            conn = new OleDbConnection(ConnectionString);
+        }
+
+        void Tourists()
+        {
+            commandText = "SELECT * FROM Туристы";
+            conn.Open();
+            MyCommand = new OleDbCommand();
+            MyCommand.Connection = conn;
+            MyCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = MyCommand;
+            ds = new DataSet();
+            dataAdapter.Fill(ds, "Туристы");
+            dataGridView1.DataSource = ds.Tables["Туристы"].DefaultView;
+            conn.Close();
+        }
+
+        private void туристыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Tourists();
+        }
+
+        void Tours()
+        {
+            commandText = "SELECT * FROM Туры";
+            conn.Open();
+            MyCommand = new OleDbCommand();
+            MyCommand.Connection = conn;
+            MyCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = MyCommand;
+            ds = new DataSet();
+            dataAdapter.Fill(ds, "Туры");
+            dataGridView1.DataSource = ds.Tables["Туры"].DefaultView;
+            conn.Close();
+        }
+
+        private void турыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Tours();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "Insert into Туристы (Фамилия,Имя,Отчество) values (@fName,@lName,@pName)";
+            MyCommand = new OleDbCommand(query, conn);
+            MyCommand.Parameters.AddWithValue("@fName", "Шмель");
+            MyCommand.Parameters.AddWithValue("@lName", "Елена");
+            MyCommand.Parameters.AddWithValue("@pName", "Андреевна");
+            conn.Open();
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+            Tourists();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "Update Туристы Set Фамилия=@fName,Имя=@lName,Отчество=@pName Where Код_туриста=@id";
+            MyCommand = new OleDbCommand(query, conn);
+            MyCommand.Parameters.AddWithValue("@fName", "Иванов");
+            MyCommand.Parameters.AddWithValue("@lName", "Григорий");
+            MyCommand.Parameters.AddWithValue("@pName", "Валентинович");
+            MyCommand.Parameters.AddWithValue("@id", 1);
+            conn.Open();
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+            Tourists();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From Туры Where Код_тура=@id";
+            MyCommand = new OleDbCommand(query, conn);
+            MyCommand.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells[0].Value);
+            conn.Open();
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+            Tours();
+        }
+    }
+}
