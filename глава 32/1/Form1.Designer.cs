12d59434699fc639768df8a6db44a21e33f3e27f
InnaSad <innasadkovskaa@gmail.com>
2022-06-18 10:57:17 +0300

    глава 32

---
+++ глава 32/1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.туристыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОТуристахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dBTur_firmDataSet = new _1.DBTur_firmDataSet();
+            this.dBTurfirmDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.турыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.путевкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сезоныToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оплатаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(893, 304);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.туристыToolStripMenuItem,
+            this.информацияОТуристахToolStripMenuItem,
+            this.турыToolStripMenuItem,
+            this.путевкиToolStripMenuItem,
+            this.сезоныToolStripMenuItem,
+            this.оплатаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(912, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // туристыToolStripMenuItem
+            // 
+            this.туристыToolStripMenuItem.Name = "туристыToolStripMenuItem";
+            this.туристыToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.туристыToolStripMenuItem.Text = "Туристы";
+            this.туристыToolStripMenuItem.Click += new System.EventHandler(this.туристыToolStripMenuItem_Click);
+            // 
+            // информацияОТуристахToolStripMenuItem
+            // 
+            this.информацияОТуристахToolStripMenuItem.Name = "информацияОТуристахToolStripMenuItem";
+            this.информацияОТуристахToolStripMenuItem.Size = new System.Drawing.Size(192, 24);
+            this.информацияОТуристахToolStripMenuItem.Text = "Информация о туристах";
+            this.информацияОТуристахToolStripMenuItem.Click += new System.EventHandler(this.информацияОТуристахToolStripMenuItem_Click);
+            // 
+            // dBTur_firmDataSet
+            // 
+            this.dBTur_firmDataSet.DataSetName = "DBTur_firmDataSet";
+            this.dBTur_firmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dBTurfirmDataSetBindingSource
+            // 
+            this.dBTurfirmDataSetBindingSource.DataSource = this.dBTur_firmDataSet;
+            this.dBTurfirmDataSetBindingSource.Position = 0;
+            // 
+            // турыToolStripMenuItem
+            // 
+            this.турыToolStripMenuItem.Name = "турыToolStripMenuItem";
+            this.турыToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.турыToolStripMenuItem.Text = "Туры";
+            this.турыToolStripMenuItem.Click += new System.EventHandler(this.турыToolStripMenuItem_Click);
+            // 
+            // путевкиToolStripMenuItem
+            // 
+            this.путевкиToolStripMenuItem.Name = "путевкиToolStripMenuItem";
+            this.путевкиToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.путевкиToolStripMenuItem.Text = "Путевки";
+            this.путевкиToolStripMenuItem.Click += new System.EventHandler(this.путевкиToolStripMenuItem_Click);
+            // 
+            // сезоныToolStripMenuItem
+            // 
+            this.сезоныToolStripMenuItem.Name = "сезоныToolStripMenuItem";
+            this.сезоныToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.сезоныToolStripMenuItem.Text = "Сезоны";
+            this.сезоныToolStripMenuItem.Click += new System.EventHandler(this.сезоныToolStripMenuItem_Click);
+            // 
+            // оплатаToolStripMenuItem
+            // 
+            this.оплатаToolStripMenuItem.Name = "оплатаToolStripMenuItem";
+            this.оплатаToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.оплатаToolStripMenuItem.Text = "Оплата";
+            this.оплатаToolStripMenuItem.Click += new System.EventHandler(this.оплатаToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 340);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Задание 1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTur_firmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBTurfirmDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dBTurfirmDataSetBindingSource;
+        private DBTur_firmDataSet dBTur_firmDataSet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem туристыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОТуристахToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem турыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem путевкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сезоныToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оплатаToolStripMenuItem;
+    }
+}
+
---
+++ глава 32/1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand myCommand;
+        OleDbDataAdapter dataAdapter;
+        string commandText;
+        string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=DBTur_firm.mdb";
+            conn = new OleDbConnection(connectionString);
+        }
+
+        private void туристыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM Туристы";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsTourists = new DataSet();
+            DataTable dtTourists = dsTourists.Tables.Add("Туристы");
+            DataColumn dсIDtourist = dtTourists.Columns.Add("Код_туриста", typeof(Int32));
+            dсIDtourist.Unique = true;
+            DataColumn dcSurame = dtTourists.Columns.Add("Фамилия");
+            DataColumn dcName = dtTourists.Columns.Add("Имя");
+            DataColumn dcPatronymic = dtTourists.Columns.Add("Отчество");
+            conn.Open();
+            dataAdapter.Fill(dsTourists.Tables["Туристы"]);
+            conn.Close();
+            dataGridView1.DataSource = dsTourists.Tables["Туристы"].DefaultView;
+        }
+
+        private void турыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM Туры";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsTours = new DataSet();
+            DataTable dtTours = dsTours.Tables.Add("Туры");
+            DataColumn dсIDtour = dtTours.Columns.Add("Код_тура", typeof(Int32));
+            dсIDtour.Unique = true;
+            DataColumn dcName = dtTours.Columns.Add("Название");
+            DataColumn dcPrice = dtTours.Columns.Add("Цена", typeof(Decimal));
+            DataColumn dcInformation = dtTours.Columns.Add("Информация");
+            conn.Open();
+            dataAdapter.Fill(dsTours.Tables["Туры"]);
+            conn.Close();
+            dataGridView1.DataSource = dsTours.Tables["Туры"].DefaultView;
+        }
+
+        private void информацияОТуристахToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM [Информация о туристах]";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsInformation = new DataSet();
+            DataTable dtInformation = dsInformation.Tables.Add("Информация о туристах");
+            DataColumn dсIDtourist = dtInformation.Columns.Add("Код туриста", typeof(Int32));
+            dсIDtourist.Unique = true;
+            DataColumn dcPassport = dtInformation.Columns.Add("Серия паспорта");
+            DataColumn dcCity = dtInformation.Columns.Add("Город");
+            DataColumn dcCountry = dtInformation.Columns.Add("Страна");
+            DataColumn dcPhone = dtInformation.Columns.Add("Телефон");
+            DataColumn dсIndex = dtInformation.Columns.Add("Индекс", typeof(Int32));
+            conn.Open();
+            dataAdapter.Fill(dsInformation.Tables["Информация о туристах"]);
+            conn.Close();
+            dataGridView1.DataSource = dsInformation.Tables["Информация о туристах"].DefaultView; 
+        }
+
+        private void путевкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM Путевки";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsTravel = new DataSet();
+            DataTable dtTravel = dsTravel.Tables.Add("Путевки");
+            DataColumn dсIDtravel = dtTravel.Columns.Add("Код путевки", typeof(Int32));
+            dсIDtravel.Unique = true;
+            DataColumn dcIDtourist = dtTravel.Columns.Add("Код туриста", typeof(Int32));
+            dcIDtourist.Unique = true;
+            DataColumn dcIDSeason = dtTravel.Columns.Add("Код сезона", typeof(Int32));
+            dcIDSeason.Unique = true;
+            conn.Open();
+            dataAdapter.Fill(dsTravel.Tables["Путевки"]);
+            conn.Close();
+            dataGridView1.DataSource = dsTravel.Tables["Путевки"].DefaultView;
+        }
+
+        private void сезоныToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM Сезоны";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsSeason = new DataSet();
+            DataTable dtSeason = dsSeason.Tables.Add("Сезоны");
+            DataColumn dсIDseason = dtSeason.Columns.Add("Код сезона", typeof(Int32));
+            dсIDseason.Unique = true;
+            DataColumn dcIDtour = dtSeason.Columns.Add("Код тура", typeof(Int32));
+            dcIDtour.Unique = true;
+            DataColumn dcDateOfStart = dtSeason.Columns.Add("Дата начала", typeof(DateTime));
+            DataColumn dcDateOfEnd = dtSeason.Columns.Add("Дата конца", typeof(DateTime));
+            DataColumn dcSeasonClose = dtSeason.Columns.Add("Сезон закрыт", typeof(Boolean));
+            DataColumn dсNumber = dtSeason.Columns.Add("Количество мест", typeof(Int32));
+            conn.Open();
+            dataAdapter.Fill(dsSeason.Tables["Сезоны"]);
+            conn.Close();
+            dataGridView1.DataSource = dsSeason.Tables["Сезоны"].DefaultView;
+        }
+
+        private void оплатаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commandText = "SELECT * FROM Оплата";
+            myCommand = new OleDbCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = commandText;
+            dataAdapter = new OleDbDataAdapter();
+            dataAdapter.SelectCommand = myCommand;
+            DataSet dsPayment = new DataSet();
+            DataTable dtPayment = dsPayment.Tables.Add("Оплата");
+            DataColumn dсIDpayment = dtPayment.Columns.Add("Код оплаты", typeof(Int32));
+            dсIDpayment.Unique = true;
+            DataColumn dcIDtravel = dtPayment.Columns.Add("Код путевки", typeof(Int32));
+            dcIDtravel.Unique = true;
+            DataColumn dcDateOfPayment = dtPayment.Columns.Add("Дата оплаты", typeof(DateTime));
+            DataColumn dcSumma = dtPayment.Columns.Add("Сумма", typeof(Decimal));
+            conn.Open();
+            dataAdapter.Fill(dsPayment.Tables["Оплата"]);
+            conn.Close();
+            dataGridView1.DataSource = dsPayment.Tables["Оплата"].DefaultView;
+        }
+    }
+}
